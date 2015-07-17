42d3c3562017aace28b70cf873db2eeadc2e656e
xuguoliang <xuguoliang@3dbox.com>
2015-07-17 18:04:18 +0800

    New: drop_file_dec

---
+++ qtkit/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from drag_move_dec import drag_move_dec
---
+++ qtkit/drop_file_dec.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# title       :
+# description :
+# author      :'Aaron'
+# mtine       :''
+# version     :
+# usage       :
+# notes       :
+
+# Built-in modules
+import os
+
+# Third-party modules
+import PyQt4.QtCore as QtCore
+
+# Studio modules
+
+# Local modules
+
+
+def drag_move_dec(widget_class):
+    """
+    A decorator offers drop file event functions for a qwidget class
+    :param widget_class:
+    :return MovedClass:
+    """
+
+    class DropClass(widget_class):
+        droped_in = QtCore.pyqtSignal(list)
+
+        def __init__(self, *args, **kwargs):
+            super(DropClass, self).__init__(*args, **kwargs)
+            self.setAcceptDrops(True)
+            self.wrapped = widget_class()
+
+        def dragEnterEvent(self, event):
+            if event.mimeData().hasUrls():
+                event.acceptProposedAction()
+            else:
+                event.ignore()
+
+        def dragMoveEvent(self, event):
+            if event.mimeData().hasUrls:
+                event.acceptProposedAction()
+            else:
+                event.ignore()
+
+        def dropEvent(self, event):
+            in_paths = list()
+            for url in event.mimeData().urls():
+                path = url.toLocalFile()
+                in_paths.append(path)
+            self.droped_in.emit(in_paths)
+
+    return DropClass
+
+if __name__ == "__main__":
+    import PyQt4.QtGui as QtGui
+    import sys
+
+    @drag_move_dec
+    class TestLabel(QtGui.QLabel):
+        pass
+
+    def print_files(file_list):
+        print "="*20
+        for f in file_list:
+            print unicode(f)
+        print "="*20
+
+    app = QtGui.QApplication(sys.argv)
+    test = TestLabel("hello")
+    test.droped_in.connect(print_files)
+    test.show()
+    app.exec_()
